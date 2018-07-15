a0629f2ff18da8c7cccb4c5d7cd7ffbcfec263a1
William Ayerst <william@ayerst.net>
2018-07-15 20:06:44 +0100

    Initial

---
+++ website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'ayerstnet-sfb.eu.auth0.com',
+        clientId: 'JoX6i2HRNpgZ5Nwy-4-QzNoUdEi9uaBi'
+    },
+    apiBaseUrl: 'https://nqtrret36m.execute-api.us-east-1.amazonaws.com/Dev'
+};
